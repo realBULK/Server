f418b7d7d6f9dda1138ff4374e8f1c57747c1030
ee h <eeh@eeui-MacBookPro.local>
2025-02-01 08:51:58 +0900

    ERROR emoji 삭제 에러 해결

--- src/main/java/umc7th/bulk/userEmoji/repository/UserEmojiRepository.java
+++ src/main/java/umc7th/bulk/userEmoji/repository/UserEmojiRepository.java
@@ -1,6 +1,7 @@
 package umc7th.bulk.userEmoji.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import umc7th.bulk.emojiRecord.entity.EmojiRecord;
 import umc7th.bulk.group.entity.Group;
 import umc7th.bulk.userEmoji.entity.UserEmoji;
 import umc7th.bulk.user.domain.User;
@@ -10,4 +11,5 @@ import java.util.List;
 public interface UserEmojiRepository extends JpaRepository<UserEmoji, Long> {
     List<UserEmoji> findByReceiver(User receiver); // 특정 사용자가 받은 이모지 조회
     int countByReceiverAndEmojiRecord_EmojiRecordIdIn(User receiver, List<Long> emojiRecordIds);
+    void deleteByEmojiRecord(EmojiRecord emojiRecord);
 }
